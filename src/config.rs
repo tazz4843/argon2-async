88f32bc9c79213caede17fb6795113a30171470f
0/0 <zero@imaskeleton.me>
2021-12-05 18:08:48 -0700

    add missing docs

--- src/config.rs
+++ src/config.rs
@@ -14,13 +14,57 @@ pub async fn set_config(config: Config<'static>) {
     }
 }
 
+/// Configuration for the `argon2` algorithm.
 pub struct Config<'k> {
+    /// Set the hashing algorithm in use.
+    ///
+    /// According to the [latest (as of 5/18) Argon2 RFC](https://tools.ietf.org/html/draft-irtf-cfrg-argon2-03) ...
+    /// "Argon2 has one primary variant: Argon2id, and two supplementary
+    /// variants: Argon2d and Argon2i. Argon2d uses data-dependent memory
+    /// access, which makes it suitable for ... applications with no threats from
+    /// side-channel timing attacks. Argon2i uses data-independent memory access,
+    /// which is preferred for password hashing and password-based key derivation.
+    /// Argon2id works as Argon2i for the first half of the first iteration over the memory,
+    /// and as Argon2d for the rest, thus providing both side-channel attack
     pub algorithm: Algorithm,
+    /// Set the version of `argon2` to use.
     pub version: Version,
+    /// The secret key to use.
+    ///
+    /// This is **strongly** recommended in production.
     pub secret_key: Option<&'k [u8]>,
+    /// The memory cost of the algorithm in kilobytes.
+    ///
+    /// Argon2 has a notion of "memory size" or "memory cost" (in kilobytes). All else equal
+    /// and generally speaking, the greater the memory cost, the longer it takes to perform
+    /// the hash and the more secure the resulting hash. More memory cost basically means
+    /// more memory used. This and "iterations" are, generally speaking, the two parameters
+    /// to adjust in order to increase or decrease the security of your hash. If you're going
+    /// to use this crate in production, you should probably tweak this parameter (and the
+    /// iterations parameter) in order to increase the time it takes to hash to the maximum
+    /// you can reasonably allow for your use-case (e.g. to probably about 300-500 milliseconds
+    /// for the use-case of hashing user passwords for a website).
     pub memory_cost: u32,
+    /// The number of iterations to use.
+    ///
+    /// Argon2 has a notion of "iterations" or "time cost". All else equal and generally
+    /// speaking, the greater the number of iterations, the longer it takes to perform the
+    /// hash and the more secure the resulting hash. More iterations basically means more
+    /// CPU load. This and "memory cost" are the two primary parameters to adjust in order
+    /// to increase or decrease the security of your hash. If you're going to use this
+    /// crate in production, you should probably tweak this parameter (and the memory cost
+    /// parameter) in order to increase the time it takes to hash to the maximum you can
+    /// reasonably allow for your use-case (e.g. to probably about 300-500 milliseconds for
+    /// the use-case of hashing user passwords for a website).
     pub iterations: u32,
+    /// The parallelism of the algorithm.
+    ///
+    /// Argon2 can break up its work into one or more "lanes" during some parts of the
+    /// hashing algorithm. If you configure it with multiple lanes, the hashing algorithm
+    /// will perform its work in parallel in some parts, potentially speeding up the time
+    /// it takes to produce a hash without diminishing the security of the result.
     pub parallelism: u32,
+    /// The output length of the algorithm in bytes.
     pub output_length: Option<usize>,
 }
 
@@ -72,16 +116,6 @@ impl<'k> Config<'k> {
     /// Set the hashing algorithm in use.
     ///
     /// The default (Argon2id) should be fine for most uses.
-    ///
-    /// According to the [latest (as of 5/18) Argon2 RFC](https://tools.ietf.org/html/draft-irtf-cfrg-argon2-03) ...
-    /// "Argon2 has one primary variant: Argon2id, and two supplementary
-    /// variants: Argon2d and Argon2i. Argon2d uses data-dependent memory
-    /// access, which makes it suitable for ... applications with no threats from
-    /// side-channel timing attacks. Argon2i uses data-independent memory access,
-    /// which is preferred for password hashing and password-based key derivation.
-    /// Argon2id works as Argon2i for the first half of the first iteration over the memory,
-    /// and as Argon2d for the rest, thus providing both side-channel attack
-    /// protection and brute-force cost savings due to time-memory tradeoffs."
     #[inline]
     pub fn set_algorithm(&mut self, algorithm: Algorithm) -> &mut Self {
         self.algorithm = algorithm;
@@ -107,16 +141,6 @@ impl<'k> Config<'k> {
     /// Set the memory cost in kilobytes.
     ///
     /// Default: 512 for insecure, and 4,096 for secure.
-    ///
-    /// Argon2 has a notion of "memory size" or "memory cost" (in kilobytes). All else equal
-    /// and generally speaking, the greater the memory cost, the longer it takes to perform
-    /// the hash and the more secure the resulting hash. More memory cost basically means
-    /// more memory used. This and "iterations" are, generally speaking, the two parameters
-    /// to adjust in order to increase or decrease the security of your hash. If you're going
-    /// to use this crate in production, you should probably tweak this parameter (and the
-    /// iterations parameter) in order to increase the time it takes to hash to the maximum
-    /// you can reasonably allow for your use-case (e.g. to probably about 300-500 milliseconds
-    /// for the use-case of hashing user passwords for a website).
     #[inline]
     pub fn set_memory_cost(&mut self, memory_cost: u32) -> &mut Self {
         self.memory_cost = memory_cost;
@@ -126,16 +150,6 @@ impl<'k> Config<'k> {
     /// Set the number of iterations to use.
     ///
     /// Default: 3 for insecure, and 200 for secure.
-    ///
-    /// Argon2 has a notion of "iterations" or "time cost". All else equal and generally
-    /// speaking, the greater the number of iterations, the longer it takes to perform the
-    /// hash and the more secure the resulting hash. More iterations basically means more
-    /// CPU load. This and "memory cost" are the two primary parameters to adjust in order
-    /// to increase or decrease the security of your hash. If you're going to use this
-    /// crate in production, you should probably tweak this parameter (and the memory cost
-    /// parameter) in order to increase the time it takes to hash to the maximum you can
-    /// reasonably allow for your use-case (e.g. to probably about 300-500 milliseconds for
-    /// the use-case of hashing user passwords for a website).
     #[inline]
     pub fn set_iterations(&mut self, iterations: u32) -> &mut Self {
         self.iterations = iterations;
@@ -145,11 +159,6 @@ impl<'k> Config<'k> {
     /// Set the parallelism of the algorithm.
     ///
     /// Default: 1 for insecure, and the number of physical CPU cores on the host for secure.
-    ///
-    /// Argon2 can break up its work into one or more "lanes" during some parts of the
-    /// hashing algorithm. If you configure it with multiple lanes, the hashing algorithm
-    /// will perform its work in parallel in some parts, potentially speeding up the time
-    /// it takes to produce a hash without diminishing the security of the result.
     #[inline]
     pub fn set_parallelism(&mut self, parallelism: u32) -> &mut Self {
         self.parallelism = parallelism;
--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,14 @@
 //!
 //! [`argon2`]: (argon2)
 
+#![deny(missing_docs)]
+
 mod config;
 mod error;
 mod hasher;
 mod spawn_task;
 
+/// A type helper for every result returned by this crate.
 pub type Result<T> = std::result::Result<T, Error>;
 
 pub use config::*;
